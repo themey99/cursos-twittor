5d7919e7946c3f3223f06b6945657aac88ca4bf2
Meiyer Jaimes <meyerjaimes123@gmail.com>
2020-11-15 13:14:51 -0400

    Modificaciones de estilos

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
 importScripts('js/sw-utils.js')
 
 const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -46,6 +46,9 @@ self.addEventListener('activate', e => {
   const response = caches.keys().then(keys => {
     keys.forEach(key => {
       if (key !== STATIC_CACHE && key.includes('static')) {
+        return caches.delete(key);
+      }
+      if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
         return caches.delete(key);
       }
     })
