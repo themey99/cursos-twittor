3e4578dd31bbab2c7b403830fa60804d091c07b0
Meiyer Jaimes <meyerjaimes123@gmail.com>
2020-11-15 12:42:02 -0400

    Primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,11 @@
+function updateDynamicCache(dynamicCache, request, response) {
+  if (response.ok) {
+    return caches.open(dynamicCache).then(cache => {
+      cache.put(request, response.clone());
+
+      return response.clone();
+    });
+  } else {
+    return response;
+  }
+}
